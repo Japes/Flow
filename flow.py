e2efa3663bb232f87ba5b1b4f0be4c983dcce36a
JP da Conceicao <jpdacon@gmail.com>
2018-10-23 21:58:00 +0200

    Got 3 columns of boxes going

--- flow.py
+++ flow.py
@@ -1,9 +1,13 @@
 from __future__ import division, print_function, unicode_literals
 
+#cocos stuff
 import cocos
 import cocos.actions as ac
 from cocos.sprite import Sprite
 from pyglet.window import key
+
+#other stuff
+import random
 
 consts = {
     "window": {
@@ -21,11 +25,7 @@ consts = {
         "topSpeed": 100.0,
         "angular_velocity": 240.0,  # degrees / s
         "accel": 85.0,
-        "bindings": {
-            key.LEFT: 'left',
-            key.RIGHT: 'right',
-            key.UP: 'up',
-        }
+
     },
     "view": {
         # as the font file is not provided it will decay to the default font;
@@ -41,8 +41,12 @@ consts = {
     }
 }
 
-g_hitBoxHeight = consts["window"]["height"]/5
+g_screenHeight = consts["window"]["height"]
+g_screenWidth = consts["window"]["width"]
+g_hitBoxHeight = g_screenHeight/5
 
+g_keyBindings =     [key.LEFT,          key.DOWN,           key.RIGHT           ]     # the buttons in order from left to right
+g_hitBoxPositions = [g_screenWidth/3,   g_screenWidth/2,    2*g_screenWidth/3   ]     # x coords of hitboxes in order from left to right
 
 class HitBox(cocos.sprite.Sprite):
     def __init__(self, x, y):
@@ -52,9 +56,9 @@ class HitBox(cocos.sprite.Sprite):
 class HitBoxes(cocos.layer.Layer):
     def __init__(self):
         super(HitBoxes, self).__init__()
-        sprite = HitBox(consts["window"]["width"]/2, g_hitBoxHeight)
-        self.add(sprite)
-        #spriteWidth = sprite.get_rect().size[0]
+        for pos in g_hitBoxPositions:
+            sprite = HitBox(pos, g_hitBoxHeight)
+            self.add(sprite)
 
 class FallingBox(cocos.sprite.Sprite):
     def __init__(self, x, y):
@@ -65,29 +69,36 @@ class FallingBox(cocos.sprite.Sprite):
 
 class FallingBoxes(cocos.layer.Layer):
     is_event_handler = True
-    boxes = set()
-
 
     def __init__(self):
         super(FallingBoxes, self).__init__()
-        self.schedule_interval(self.update, 1)
+        self.schedule_interval(self.generateBox, 1)
+        self.boxes = []
+        for col in g_keyBindings:
+            self.boxes.append([])
+
         self.text = cocos.text.Label("", x=100, y=280)
         self.add(self.text)
 
-    def update(self, dt):
-        
-        sprite = FallingBox(consts["window"]["width"]/2, consts["window"]["height"]*1.25)
+    #called by scheduler
+    def generateBox(self, dt):
+        random_index = random.randrange(len(g_keyBindings))
+        sprite = FallingBox(g_hitBoxPositions[random_index], consts["window"]["height"]*1.25)
         self.add(sprite)
+        self.boxes[random_index].append(sprite)
 
     def on_key_press (self, k, modifiers):
-        if (k == key.DOWN):
-            children = self.get_children()
+        if (k in g_keyBindings):
+            children = self.boxes[g_keyBindings.index(k)]
             bestOne = min([abs(child.y - g_hitBoxHeight) for child in children])
             self.text.element.text = str(bestOne)
-            bestChild = [child for child in children if abs(child.y - g_hitBoxHeight) == bestOne] #yuck
+            bestChild = [child for child in children if abs(child.y - g_hitBoxHeight) == bestOne][0] #yuck
             if(bestChild and bestOne < 10):
-                self.remove(bestChild[0])
+                action = ac.FadeOut(0.25) + ac.CallFuncS(self.remove_child)
+                bestChild.do(action)
 
+    def remove_child(self, sprite):
+        self.remove(sprite)
 
 if __name__ == "__main__":
     # director init takes the same arguments as pyglet.window
